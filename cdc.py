047b47bb4be842e2621e4720a4c0d8fcf2fe0291
Oaggour <oaggour@umich.edu>
2024-12-06 01:58:51 -0500

    create_table & get_last_index

--- cdc.py
+++ cdc.py
@@ -30,3 +30,37 @@ def fetch_data():
     else:
         print(f"Failed to fetch data: {response.status_code}")
         return []
+
+def create_table(conn):
+    """
+    Creates the `er_data` table if it doesn't exist.
+    Args:
+        conn: SQLite connection object.
+    """
+    create_table_sql = f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        county_fips TEXT,
+        covid_hospital_admissions_per_100k REAL,
+        covid_19_community_level TEXT
+    );
+    """
+    conn.execute(create_table_sql)
+    conn.commit()
+
+def get_last_index(conn):
+    """
+    Gets the last index inserted in the database.
+    Args:
+        conn: SQLite connection object.
+    Returns:
+        int: Last index inserted in the database.
+    """
+    query_sql = f"SELECT MAX(id) FROM {TABLE_NAME};"
+    cursor = conn.execute(query_sql)
+    result = cursor.fetchone()
+    
+    if result and result[0]:
+        return result[0]
+    else:
+        return 0
